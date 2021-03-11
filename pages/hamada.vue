e7d2d36b95230daae672780ebdc3003b71cd67a2
Kazutaka Hamada <hama0102105@gmail.com>
2021-03-11 21:26:43 +0900

    add:typescript

---
+++ pages/hamada.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+.hoge(v-cloak)
+  p テスト
+  button(@click="hogeFunc2('hogeFunc2')") {{ hogeStr }}
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from "@vue/composition-api"
+
+/**
+ * ダッシュボード画面
+ */
+export default defineComponent({
+  components: {},
+  props: {},
+  setup() {
+    
+    const hogeStr:string = "ボタンです";
+    const hogeNum:number = 123
+    
+    const hogeFunc2 = (message: string) => {
+      console.log(message);
+    };
+    
+    onMounted(()=> {
+    })
+    
+    return {
+      hogeStr,
+      hogeFunc2
+    };
+  }
+});
+</script>
+
+<style lang="scss"></style>
