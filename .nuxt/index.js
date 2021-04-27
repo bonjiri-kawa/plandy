58b48f32f4c36d2dc9798c4b8f3108669bc64ca1
Kazutaka Hamada <hama0102105@gmail.com>
2021-04-27 23:15:55 +0900

    nuxt.config.tsにvue2-google-mapsを登録

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -15,6 +15,7 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 import nuxt_plugin_plugin_460056d5 from 'nuxt_plugin_plugin_460056d5' // Source: ./components/plugin.js (mode: 'all')
 import nuxt_plugin_axios_6676596b from 'nuxt_plugin_axios_6676596b' // Source: ./axios.js (mode: 'all')
 import nuxt_plugin_compositionapi_fa0591fa from 'nuxt_plugin_compositionapi_fa0591fa' // Source: ../plugins/composition-api (mode: 'all')
+import nuxt_plugin_vue2googlemaps_4309cabe from 'nuxt_plugin_vue2googlemaps_4309cabe' // Source: ../plugins/vue2-google-maps (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -184,6 +185,10 @@ async function createApp(ssrContext, config = {}) {
 
   if (typeof nuxt_plugin_compositionapi_fa0591fa === 'function') {
     await nuxt_plugin_compositionapi_fa0591fa(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_vue2googlemaps_4309cabe === 'function') {
+    await nuxt_plugin_vue2googlemaps_4309cabe(app.context, inject)
   }
 
   // Lock enablePreview in context
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,10 @@ const nuxtConfig: Configuration = {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/composition-api'],
+  plugins: [
+    '@/plugins/composition-api',
+    '@/plugins/vue2-google-maps'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
