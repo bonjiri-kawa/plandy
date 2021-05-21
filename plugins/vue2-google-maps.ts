e97c7eb86babebe546e7d3183900b80677688f96
PC-445\kawashima <kawashima_takumi@comsq.com>
2021-05-21 11:03:22 +0900

    git chache clear

--- plugins/vue2-google-maps.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyDVsndM5LyMAb5If-6D9T2rw0myLdlC1Vk',
-    libraries: 'places',
-  },
-})
