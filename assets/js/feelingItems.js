517527df0c038f49eaf01e8373439bd38b32141a
Kazutaka Hamada <hama0102105@gmail.com>
2022-05-01 10:05:19 +0900

    気分で検索

---
+++ assets/js/feelingItems.js
@@ -0,0 +1,47 @@
+export const defaultFeelingItems = [
+  {
+    feeling: "まったりしたい",
+    keyword:["zoo", "rv_park", "movie_rental", "florist", "movie_theater", "hair_care", "church", "aquarium", "shoe_store", "museum", "spa", "bar", "bakery"]
+  },
+  {
+    feeling: "ワクワクしたい",
+    keyword:["amusement_park", "casino", "movie_rental", "rv_park", "gym", "movie_theater", "tourist_attraction", "aquarium", "museum"]
+  },
+  {
+    feeling: "感動したい",
+    keyword: ["zoo", "movie_rental", "movie_theater", "tourist_attraction", "museum"]
+  },
+  {
+    feeling: "新しい刺激が欲しい",
+    keyword: ["mosque", "rv_park", "casino", "movie_rental", "gym", "movie_theater", "tourist_attraction", "amusement_park", "embassy", "liquor_store", "church", "museum", "night_club", "stadium"]
+  },
+  {
+    feeling: "体を動かしたい",
+    keyword: ["gym", "campground", "night_club"]
+  },
+  {
+    feeling: "笑いたい",
+    keyword: ["movie_rental", "movie_theater"]
+  },
+  {
+    feeling: "癒されたい",
+    keyword: ["zoo", "florist", "campground", "amusement_park", "hair_care", "aquarium", "pet_store", "park", "spa"]
+  },
+  {
+    feeling: "自然に触れたい",
+    keyword: ["zoo", "rv_park", "florist", "campground", "tourist_attraction", "aquarium", "park"]
+  },
+  {
+    feeling: "アブノーマルがいい",
+    keyword: ["mosqu", "rv_park", "casino", "embassy", "church", "synagogue", "hardware_store", "beauty_salon", "night_club", "stadium", "courthouse"]
+  },
+  {
+    feeling: "学びたい",
+    keyword: ["mosque", "casino", "movie_rental", "church", "library", "book_store", "museum", "hindu_temple"]
+  },
+  {
+    feeling: "ウィンドウショッピングがしたい",
+    keyword: ["department_store", "supermarket", "hardware_store", "shopping_mall", "clothing_store", "home_goods_store", "jewelry_store"]
+  }
+]
+
